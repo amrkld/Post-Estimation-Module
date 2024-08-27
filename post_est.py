1f74b8701420b4568540538499e5a4618224cc29
amrkld <amrk6668@gmail.com>
2024-08-27 15:12:24 +0300

    Add comments  to the code

--- post_est.py
+++ post_est.py
@@ -2,8 +2,14 @@ import cv2
 import mediapipe as mp
 import time
 
-# Initialize pose detection or any other modules you use (Assuming these are defined somewhere above in your code)
-cap = cv2.VideoCapture("videos/video_01.mp4")  # Assuming you are capturing from a webcam
+
+cap = cv2.VideoCapture("videos/video_01.mp4")
+
+"""
+# We can cap the video from a webcam by
+cap = cv2.VideoCapture(0)
+
+"""
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 mpDraw = mp.solutions.drawing_utils
@@ -22,24 +28,28 @@ while(True):
     if results.pose_landmarks:
         mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
 
+        #customize circles
         for id, lm in enumerate(results.pose_landmarks.landmark):
             h, w, c = img.shape
             cx, cy = int(lm.x * w), int(lm.y * h)
             cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
 
+
+    #calc and print the frame rate
     ctime = time.time()
     fps = 1 / (ctime - pTime)
     pTime = ctime
-
     cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
 
     cv2.imshow("Image", img)
 
-    # Check for user input to break the loop
+
+    #If clicks esc key
     if cv2.waitKey(1) & 0xFF == 27:  # 27 is the ASCII code for the Esc key
         break
     
-    # Alternatively, check if the window is closed
+    #check if the window is closed
     if cv2.getWindowProperty('Image', cv2.WND_PROP_VISIBLE) < 1:
         break
 
