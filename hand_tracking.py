437b62ec1c168724299ad590198d7894eae3a1c8
amrkld <amrk6668@gmail.com>
2024-08-28 14:14:09 +0300

    Add drawing circle to a specific point in the arm

--- hand_tracking.py
+++ hand_tracking.py
@@ -17,6 +17,11 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x *w) ,int(lm.y *h)
+                if id == 4:
+                    cv2.circle(img, (cx, cy), 15, (225, 0, 225), cv2.FILLED)
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
     #calc and print the frame rate
